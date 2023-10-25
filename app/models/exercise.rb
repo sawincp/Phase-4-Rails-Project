196144cb2c7f5313478b16f6ca4f88ceaa4d8a78
Corey Sawin <sawincp@gmail.com>
2023-10-25 12:48:30 -0400

    update presence exercise validator

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -3,7 +3,7 @@ class Exercise < ApplicationRecord
 
     has_many :users, through: :workouts
 
-    validates :name, pressence: {message: "Please enter the exercise"}
+    validates :name, presence: {message: "Please enter the exercise"}
     validates :weight, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'Please enter a valid weight' }
     validates :reps, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'Please enter a valid number of reps' }
 
