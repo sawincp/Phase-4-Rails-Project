40793424b06daa760a1dd942161ae7adc62c35ce
Corey Sawin <sawincp@gmail.com>
2023-10-25 12:44:22 -0400

    Add exercise validations and seed data

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -3,4 +3,9 @@ class Exercise < ApplicationRecord
 
     has_many :users, through: :workouts
 
+    validates :name, pressence: {message: "Please enter the exercise"}
+    validates :weight, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'Please enter a valid weight' }
+    validates :reps, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'Please enter a valid number of reps' }
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Seeding data..."
+
+Exercise.create(name: "Bench Press")
+Exercise.create(name: "Squat")
+Exercise.create(name: "Deadlift")
+Exercise.create(name: "Kettlebell Swing")
+Exercise.create(name: "Push Up")
+Exercise.create(name: "Sit Up")
+Exercise.create(name: "Lunges")
+Exercise.create(name: "Pull Ups")
+Exercise.create(name: "Jump Squats")
+Exercise.create(name: "Box Jumps")
+
+puts "Done seeding ✅ "
