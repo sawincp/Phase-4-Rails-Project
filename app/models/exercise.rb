523cb4bb0bdd635e76f411040ff751e644a0ac60
Corey Sawin <sawincp@gmail.com>
2023-10-26 11:18:27 -0400

    update models and validations

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,11 +1,7 @@
 class Exercise < ApplicationRecord
     has_many :workouts
-
     has_many :users, through: :workouts
-
+    
     validates :name, presence: {message: "Please enter the exercise"}
-    validates :weight, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'Please enter a valid weight' }
-    validates :reps, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'Please enter a valid number of reps' }
-    validates :sets, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'Please enter a valid number of sets' }
-
+   
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,9 @@
 class Workout < ApplicationRecord
     belongs_to :user
     belongs_to :exercise
+
+    validates :weight, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'Please enter a valid weight' }
+    validates :reps, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'Please enter a valid number of reps' }
+    validates :sets, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'Please enter a valid number of sets' }
+
 end
